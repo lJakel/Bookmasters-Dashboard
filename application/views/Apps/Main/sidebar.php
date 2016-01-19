4b15f715f20ee13acabdebf212e840d591b8978b
lJakel <jihasz@bookmasters.com>
2016-01-19 10:01:00 -0500

    Test

--- application/views/Apps/Main/sidebar.php
+++ application/views/Apps/Main/sidebar.php
@@ -1,6 +1,6 @@
 <span class="listheader">Dashboard</span>
 <ul class="application-items">
-   <li><a data-ui-sref="bm.app.page({app:'newtitle', page: 'submit' })"><span class="fa fa-calendar"></span>Calendar</a></li>
-   <li><a data-ui-sref="bm.app.page({app:'newtitle', page: 'view' })"><span class="fa fa-sticky-note-o"></span>Notes</a></li>
-   <li><a data-ui-sref="bm.app.page({app:'newtitle', page: 'view' })"><span class="fa fa-pencil"></span>Settings</a></li>
+   <li><a data-ui-sref="bm.app.page({app:'TitleManagement', page: 'submit' })"><span class="fa fa-calendar"></span>Calendar</a></li>
+   <li><a data-ui-sref="bm.app.page({app:'TitleManagement', page: 'view' })"><span class="fa fa-sticky-note-o"></span>Notes</a></li>
+   <li><a data-ui-sref="bm.app.page({app:'TitleManagement', page: 'view' })"><span class="fa fa-pencil"></span>Settings</a></li>
 </ul>

4e0782139f90a9483e10b5a70f55e59cafd530ab
lJakel <jihasz@bookmasters.com>
2016-01-25 12:11:18 -0500

    Bug

---
+++ application/views/Apps/Utilities/ISBNConversion.php
@@ -0,0 +1,10 @@
+<div class="row" ng-controller="UtilitiesISBNConversion as uic">
+   <div class="col-md-12">
+      <div class="panel panel-default">
+         <div class="panel-body">
+
+         </div>
+      </div>
+   </div>
+</div>
+<script type="text/javascript-lazy" data-append="partial" data-src="assets/js/views/Apps/Utilities/ISBNConversion.js?cache=<?php echo rand(1000, 9000); ?>"></script>

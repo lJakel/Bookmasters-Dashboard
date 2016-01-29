7133313ddd01cd54892dfac3a72ed060447a4163
lJakel <jihasz@bookmasters.com>
2016-01-29 14:46:55 -0500

    Overhaul

--- application/controllers/Sales/Reporting.php
+++ application/controllers/Sales/Reporting.php
@@ -2,7 +2,7 @@
 
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class SalesReports extends Secure_Controller {
+class Reporting extends Secure_Controller {
 
    var $app = [];
    var $folder = 'Apps/';
@@ -12,14 +12,17 @@ class SalesReports extends Secure_Controller {
       $this->load->model('SalesTools/SalesTools_Modal', 'st_model');
       $this->app['links'] = get_class_methods($this);
       $this->app['appName'] = get_class($this);
+      $this->app['viewFolder'] = 'Applications';
+      $this->app['folder'] = 'Sales';
    }
 
-   public function index() {
-      $this->load->view($this->folder . get_class($this) . '/' . __FUNCTION__);
+   public function Index() {
+      $this->load->view("{$this->app['viewFolder']}/{$this->app['folder']}/{$this->app['appName']}/" . __FUNCTION__);
    }
-   public function top100() {
-      
-      $this->load->view($this->folder . get_class($this) . '/' . __FUNCTION__);
+
+   public function Top100() {
+
+      $this->load->view("{$this->app['viewFolder']}/{$this->app['folder']}/{$this->app['appName']}/" . __FUNCTION__);
    }
 
    public function api() {
---
+++ application/views/Applications/Sales/Reporting/Top100.php
@@ -0,0 +1,82 @@
+<div class="row" ng-controller="Top100 as t">
+   <div class="col-md-12">
+      <div class="panel panel-default">
+         <div class="panel-body">
+
+            <!-- Nav tabs -->
+            <ul class="nav nav-tabs" role="tablist">
+               <li role="presentation" class="active"><a href="#home" aria-controls="home" role="tab" data-toggle="tab">Top 100</a></li>
+               <!--<li role="presentation"><a href="#profile" aria-controls="profile" role="tab" data-toggle="tab">Profile</a></li>-->
+            </ul>
+
+            <!-- Tab panes -->
+            <div class="tab-content">
+               <div role="tabpanel" class="tab-pane active" id="home">
+
+
+
+                  <div tasty-table bind-resource-callback="t.getResource" bind-init="t.config.init" bind-theme="t.config.theme">
+                     <table class="table table-striped table-bordered table-condensed">
+                        <thead tasty-thead></thead>
+                        <tbody>
+                           <tr ng-repeat="row in rows">
+                              <td>{{row.Isbn13}}</td>
+                              <td>{{row.Title}}</td>
+                              <td>{{row.ProductBinding}}</td>
+                              <td>{{row.DataType}}</td>
+                              <td>{{row.UnitRank}}</td>
+                              <td>{{row.Units}}</td>
+                              <td>{{row.NetSales}}</td>
+                              <td>{{row.Inventory}}</td>
+                              <td>{{row.OnOrder}}</td>
+                              <td>{{row.LastUpdated}}</td>
+                           </tr>
+                        </tbody>
+                     </table>
+                     <div tasty-pagination></div>
+                  </div>
+
+
+
+               </div>
+            </div>
+         </div>
+      </div>
+   </div>
+</div>
+<script>
+   BMApp.register.controller('Top100', ['$http', function ($http) {
+         var self = this;
+         self.config = {
+            init: {
+               'count': 20,
+               'page': 1,
+               'sortBy': 'Isbn13',
+               'sortOrder': 'desc',
+               'filterBase': 1 // set false to disable
+            },
+            theme: {
+               bootstrapIcon: false,
+               bindOnce: true,
+               loadOnInit: true,
+               iconUp: 'fa fa-sort-up',
+               iconDown: 'fa fa-sort-down',
+               itemsPerPage: 15,
+               listItemsPerPage: [5, 25, 50, 100, 200],
+            }
+         };
+         self.getResource = function (params, paramsObj) {
+            var urlApi = 'sales/api';
+            return $http.post(urlApi, paramsObj).then(function (response) {
+               console.log(response);
+               return {
+                  'rows': response.data.data.rows,
+                  'header': response.data.data.header,
+                  'pagination': response.data.data.pagination,
+                  'sortBy': response.data.data['sort-by'],
+                  'sortOrder': response.data.data['sort-order']
+               }
+            });
+         }
+      }]);
+</script>
